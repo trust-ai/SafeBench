18ed6bf51e0ae0a9b6662045d70ad1602e351556
GilgameshD <wenhaod@andrew.cmu.edu>
2023-01-25 19:46:47 -0500

    clean useless files

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+'''
+Author:
+Email: 
+Date: 2023-01-25 19:36:50
+LastEditTime: 2023-01-25 19:44:06
+Description: 
+'''
+
 from setuptools import setup, find_packages
 
-setup(name='red_team',
+setup(name='Safebench',
       packages=["gym_carla", "planning"],
       include_package_data=True,
-      version='0.0.1',
+      version='1.0.0',
       install_requires=['gym', 'pygame'])
