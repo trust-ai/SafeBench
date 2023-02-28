6d41d65447c7b946618200c4fee6b9db5e7bfa2d
Chejian Xu <chejian2@illinois.edu>
2023-02-27 22:45:02 -0600

    destroy all autopilot actors

--- safebench/scenario/scenario_definition/ordinary/autopolit_background_vehicle.py
+++ safebench/scenario/scenario_definition/ordinary/autopolit_background_vehicle.py
@@ -20,49 +20,23 @@ class AutopolitBackgroundVehicle(BasicScenario):
         self.world = world
 
         self.timeout = timeout
-        self.number_of_vehicles = int(40 / config.num_scenario)
+        self.number_of_vehicles = int(60 / config.num_scenario)
         self.number_of_walkers = 0
 
     def initialize_actors(self):
         """
         Set other_actors to the superset of all scenario actors
         """
-        # actors = CarlaDataProvider.request_new_batch_actors(
-        #     'vehicle.*',
-        #     amount=self.number_of_vehicles,
-        #     spawn_points=None, autopilot=True,
-        #     random_location=True,
-        #     rolename='autopilot'
-        # )
-        # print(f'spawned {len(actors)} actors')
-        # self.other_actors = actors
-
-        # Spawn surrounding vehicles
         vehicle_spawn_points = get_valid_spawn_points(self.world)
-        print(f'{len(vehicle_spawn_points)} valid spawn points')
         count = min(self.number_of_vehicles, len(vehicle_spawn_points))
         for spawn_point in vehicle_spawn_points:
-            vehicle = self._try_spawn_random_vehicle_at(spawn_point, number_of_wheels=[4])
+            vehicle = CarlaDataProvider.request_new_actor('vehicle.*', spawn_point=spawn_point, rolename='autopilot',
+                                                          autopilot=True, random_location=False)
             if vehicle is not None:
                 count -= 1
                 self.other_actors.append(vehicle)
             if count <= 0:
                 break
-        # if count > 0:
-        #     for spawn_point in vehicle_spawn_points:
-        #         vehicle = self._try_spawn_random_vehicle_at(spawn_point, number_of_wheels=[4])
-        #         # vehicle = CarlaDataProvider.request_new_actor('vehicle.*', spawn_point=None, rolename='autopilot', autopilot=True, random_location=True)
-        #         if vehicle is not None:
-        #             count -= 1
-        #             self.other_actors.append(vehicle)
-        #         if count <= 0:
-        #             break
-        # while count > 0:
-        #     vehicle = self._try_spawn_random_vehicle_at(random.choice(vehicle_spawn_points), number_of_wheels=[4])
-        #     if vehicle is not None:
-        #         count -= 1
-        #         self.other_actors.append(vehicle)
-        print(f'spawned {len(self.other_actors)} actors')
 
         # the trigger distance will always be 0, trigger at the beginning
         self.reference_actor = self.ego_vehicle
@@ -75,62 +49,3 @@ class AutopolitBackgroundVehicle(BasicScenario):
 
     def check_stop_condition(self):
         return False
-
-    def _try_spawn_random_walker_at(self, transform):
-        """
-            Try to spawn a walker at specific transform with random bluprint.
-            Args:
-                transform: the carla transform object.
-            Returns:
-                walker_actor: Bool indicating whether the spawn is successful.
-        """
-        walker_bp = random.choice(self.world.get_blueprint_library().filter('walker.*'))
-        # set as not invencible
-        if walker_bp.has_attribute('is_invincible'):
-            walker_bp.set_attribute('is_invincible', 'false')
-        walker_actor = self.world.try_spawn_actor(walker_bp, transform)
-
-        if walker_actor is not None:
-            walker_controller_bp = self.world.get_blueprint_library().find('controller.ai.walker')
-            walker_controller_actor = self.world.spawn_actor(walker_controller_bp, carla.Transform(), walker_actor)
-            # start walker
-            walker_controller_actor.start()
-            # set walk to random point
-            walker_controller_actor.go_to_location(self.world.get_random_location_from_navigation())
-            # random max speed
-            walker_controller_actor.set_max_speed(1 + random.random())  # max speed between 1 and 2 (default is 1.4 m/s)
-        return walker_actor
-
-    def _try_spawn_random_vehicle_at(self, transform, number_of_wheels=[4]):
-        """
-            Try to spawn a surrounding vehicle at specific transform with random bluprint.
-            Args:
-                transform: the carla transform object.
-            Returns:
-                vehicle: Bool indicating whether the spawn is successful.
-        """
-        blueprint = self._create_vehicle_bluepprint('vehicle.*', number_of_wheels=number_of_wheels)
-        blueprint.set_attribute('role_name', 'autopilot')
-        vehicle = self.world.try_spawn_actor(blueprint, transform)
-        if vehicle is not None:
-            vehicle.set_autopilot()
-        return vehicle
-
-    def _create_vehicle_bluepprint(self, actor_filter, color=None, number_of_wheels=[4]):
-        """
-            Create the blueprint for a specific actor type.
-            Args:
-                actor_filter: a string indicating the actor type, e.g, 'vehicle.lincoln*'.
-            Returns:
-                bp: the blueprint object of carla.
-        """
-        blueprints = self.world.get_blueprint_library().filter(actor_filter)
-        blueprint_library = []
-        for nw in number_of_wheels:
-            blueprint_library = blueprint_library + [x for x in blueprints if int(x.get_attribute('number_of_wheels')) == nw]
-        bp = random.choice(blueprint_library)
-        if bp.has_attribute('color'):
-            if not color:
-                color = random.choice(bp.get_attribute('color').recommended_values)
-            bp.set_attribute('color', color)
-        return bp
