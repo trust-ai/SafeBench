b8f3e4459c10c2575fc386a2e9a10984550ed698
GilgameshD <wenhaod@andrew.cmu.edu>
2023-03-09 11:10:35 -0500

    remove safe_rl folder

--- safebench/agent/safe_rl/policy/__init__.py
+++
@@ -1,8 +0,0 @@
-from .pid_controller import LagrangianPIDController
-from .ppo import PPO  # , PPOLagrangian
-from .sac import SAC
-from .td3 import TD3
-from .ddpg import DDPG
-# from .sac_lag import SACLagrangian
-# from .ddpg_lag import DDPGLagrangian
-# from .td3_lag import TD3Lagrangian
